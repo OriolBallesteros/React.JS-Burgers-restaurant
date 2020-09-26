ea7e36ba808ff71a6bc741c5e1833a088ed477f1
Oriol Ballesteros <37365063+OriolBallesteros@users.noreply.github.com>
2020-09-26 21:52:01 +0200

    Update BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,63 +12,21 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 
-//const INGREDIENT_PRICES = {
-//    salad: 0.5,
-//    cheese: 0.4,
-//    meat: 1.3,
-//    bacon: 0.7
-//};
-//Used on reducer.js
-
 class burgerBuilder extends Component {
 
     state = {
-        /* ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        }, */
-        //Once we set the ingredients on the server, on the backEnd, we do not longer need this configuration here
-
-        //ingredients: null,
-        //Once the connect, and the mapStateToProps is set, we use that state, no longer this one. 
-        //totalPrice: 4,
-        //Same case as ingredients.
-        //purchasable: false,
-        //with the reducer and the function updatePurchaseState we agaoin no longer need it.
         purchasing: false,
-
-        //loading: false,
-        //error: false
-        // --> not need of it here anymore. ../actions/burgerBuilder.js takes it. 
     }
 
     componentDidMount() {
-    //    axios.get('https://react-burger-project-a8537.firebaseio.com/ingredients.json')
-    //        .then(response => {
-    //            this.setState({ ingredients: response.data });
-    //        })
-    //        .catch(error => {
-    //            this.setState({ error: true });
-    //        }); --> ../actions/burgerBuilder.js take care of this asynchronous
         this.props.onInitIngredients();
     } 
 
     updatePurchaseState(ingredients) {
-        //const ingredients = {
-        //    ...this.state.ingredients
-        //};
-        //As long as we use the state.ingredients, the state.purchasable won't change
-        //seeing the clicked-just-added ingredient, but looking to the past state.
-        //It gotta work with the just-added or just-removed ingredient, so we passed it as a paramater.
-
-
+      
         const sum = Object.keys(ingredients)
-            //Turn the object into an array
             .map(igKey => {
                 return ingredients[igKey];
-                //take the key of each one
             })
             .reduce((sum, el) => {
                 return sum + el;
@@ -78,50 +36,6 @@ class burgerBuilder extends Component {
         return sum > 0;
     }
 
-//    addIngredientHandler = (type) => {
-//        const oldCount = this.state.ingredients[type];
-//        const updatedCount = oldCount + 1;
-//        const updatedIngredients = {
-//            ...this.state.ingredients
-//        }
-//        updatedIngredients[type] = updatedCount;
-//
-//        const priceAddition = INGREDIENT_PRICES[type];
-//        const oldPrice = this.state.totalPrice;
-//        const newPrice = oldPrice + priceAddition;
-//
-//        this.setState({
-//            ingredients: updatedIngredients,
-//            totalPrice: newPrice
-//        });
-//
-//        this.updatePurchaseState(updatedIngredients);
-//    }
-//
-//removeIngredientHandler = (type) => {
-//    const oldCount = this.state.ingredients[type];
-//    if (oldCount <= 0) {
-//        return;
-//    }
-//    const updatedCount = oldCount - 1;
-//    const updatedIngredients = {
-//        ...this.state.ingredients
-//    }
-//    updatedIngredients[type] = updatedCount;
-//
-//    const priceDeduction = INGREDIENT_PRICES[type];
-//    const oldPrice = this.state.totalPrice;
-//    const newPrice = oldPrice - priceDeduction;
-//
-//    this.setState({
-//        ingredients: updatedIngredients,
-//        totalPrice: newPrice
-//    });
-//
-//    this.updatePurchaseState(updatedIngredients);
-//}
-// --> These two functions were managing the state.ingredients and the state.totalPrice. Once the reducer manage those already, we do not need these functions anymore. 
-
     purchaseHandler = () => {
         if(this.props.isAuthenticated){
             this.setState({ purchasing: true });
@@ -130,38 +44,21 @@ class burgerBuilder extends Component {
             this.props.history.push('/auth');
         }
     }
-    //Simple reminder: the sintax 'purchaseHandler(){...} will report an error because it won't read
-    //the 'this'. That's why is a good idea to use ES6, arrow function, which solves it.
-
+  
     purchaseCancelHandler = () => {
         this.setState({ purchasing: false });
     }
 
     purchaseContinueHandler = () => {
-        //const queryParams = [];
-        //for (let i in this.state.ingredients) {
-        //    queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i])); /* encodeURIComponent = transform string, int... into valid URL */
-        //}
-        //queryParams.push('price=' + this.state.totalPrice);
-        //const queryString = queryParams.join('&');
-        //
-        //this.props.history.push({
-        //    pathname: '/checkout',
-        //    search: '?' + queryString
-        //});
-        //--> With redux there's no need of query to pass info. Redux those it.
         this.props.onInitPurchase();
         this.props.history.push('/checkout');
     }
 
     render() {
         const disabledInfo = {
-            //...this.state.ingredients --> no longer used due to mapStateToProps
             ...this.props.ings
         };
-        //We transform the key of a copy of the state.ingredients into a boolean;
-        //true if there is 1 or more; false if it's 0. With that we can now able and
-        //disable the button of 'Less', according to it. 
+       
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
@@ -190,29 +87,12 @@ class burgerBuilder extends Component {
                 purchaseContinued={this.purchaseContinueHandler} />
         }
 
-        //if (this.state.loading) {
-        //    orderSummary = <Spinner />
-        //} --> burgerBuilder.js in actions folder
-
         return (
             <Auxiliary>
-                {/* We do check de workFlow: willComponentUpdate, etc... on both Modal and 
-                OrderSummary Components due to one grabs the other inside */}
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-
                 {burger}
-                {/*<Burger ingredients={this.state.ingredients} />
-                <BuildControls
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler}
-                    price={this.state.totalPrice} />*/}
-                {/*The structure of the state.ingredients since we passed it to the backend. Now every component that use ingredients might be only rendered under conditional */}
-
             </Auxiliary>
         );
     }
