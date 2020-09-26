c94a715d0e623cef17e867b24f7ba496b604c4b0
Oriol Ballesteros <37365063+OriolBallesteros@users.noreply.github.com>
2020-09-26 21:42:12 +0200

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,8 +4,6 @@ import classes from './Logo.css';
 
 const logo = (props) => (
     <div className={classes.Logo}>
-        {/* <img src={'../../assets/images/burger-logo.png'} />  --> it won't work as expectes
-        cause once we set up the project to the server, the structure of the project will */}
         <img src={burgerLogo} alt={"MyBurger"} />
     </div>
 );
