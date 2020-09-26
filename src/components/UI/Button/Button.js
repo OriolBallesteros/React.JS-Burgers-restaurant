4ecda099e85093d54df51ab50223be2c06a769fd
Oriol Ballesteros <37365063+OriolBallesteros@users.noreply.github.com>
2020-09-26 21:47:22 +0200

    Update Button.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,6 @@ import classes from './Button.css';
 
 const button = (props) => (
     <button 
-        //We have a general style for buttons (.Button) but also an extra ones (.Success and .Danger)
-        //that we can pass dinamically as easy as using an Array an then transforming into a String
-        //and with the power of 'props'
         className={[classes.Button, classes[props.btnType]].join(' ')}
         onClick={props.clicked}
         disabled={props.disabled}>
