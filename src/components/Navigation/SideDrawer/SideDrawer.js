10d98abce7ef639b8a218779294044d27b0c71c2
Oriol Ballesteros <37365063+OriolBallesteros@users.noreply.github.com>
2020-09-26 21:44:21 +0200

    Update SideDrawer.js

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -16,9 +16,6 @@ const sideDrawer = (props) => {
         <Auxiliary>
             <Backdrop show={props.open} clicked={props.closed}/>
             <div className={attachedClasses.join(' ')} onClick={props.closed}>
-                {/* As we need to adjust the logo size, we can pass the height we want as a prop to the Logo component
-                or, as in here, we can grab the Logo component called into a div with its own Logo class in its own CSS file
-                that has the desired height */}
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
