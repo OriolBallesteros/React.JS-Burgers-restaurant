8079cafdb417ba637744ddbc6ba68671b63fe693
Oriol Ballesteros <37365063+OriolBallesteros@users.noreply.github.com>
2020-09-26 21:43:42 +0200

    Update NavigationItems.test.js

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -14,25 +14,15 @@ beforeEach(() => {
 });
 
 describe('<NavigationItems />', () => {
-    //describe is a method that take two arguments. The first one is a string to identify where
-    //is the test located, the second one is the function we want to test.
-
+    
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        //as the function itself we call the method it(), which takes two arguments two.
-        //The first one, a string where we describe what we expect to happen, the second the
-        //function itself
-
-        /* const wrapper = shallow(<NavigationItems />); */ // --> as long as we are using the beforeEach, we then of course no longer need it here
-        //with the shallow() we take the component we want to test
+       
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
-        // with the expect() and all it utilities we define... well... what we expect. 
-        // In the example: the wrapper, the component testing, might have a NavigationItem inside,
-        // and might have it two times.
+       
     });
 
     it('should render three <NavigationItem /> elements if authenticated', () => {
-        /* const wrapper = shallow(<NavigationItems isAuthenticated/>); --> beforeEach takes it place */
-        wrapper.setProps({ isAuthenticated: true }); // --> we need it cause we change the wrapper for this test
+        wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
